8511636bde4061af6bf5c12dc52b46b9f832772d
ValensioCS <150872088+ValensioCS@users.noreply.github.com>
2025-12-15 10:08:34 +0700

    true ending

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jurnalku/PermintaanSaksi.dart';
+import 'package:jurnalku/exploreeeee.dart';
 
 
 
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Jurnalku',
-      home: const PermintaanSaksiPage(),
+      home: const ExploreeeePage(),
     );
   }
 }
